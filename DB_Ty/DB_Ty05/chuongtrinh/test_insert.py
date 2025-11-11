463f136f05fab4135380e30c34eb2fc83f4f5f7b
Lê Văn Tý <tylv2280@ut.edu.vn>
2025-11-11 10:20:21 +0700

    Xóa file

--- DB_Ty/DB_Ty05/chuongtrinh/test_insert.py
+++
@@ -1,9 +0,0 @@
-from common.insertdanhmuc import insert_danhmuc
-
-while True:
-    ten = input("Nhập vào tên danh mục: ")
-    mota = input("Nhập vào mô tả: ")
-    insert_danhmuc(ten, mota)
-    con = input("TIẾP TỤC Y, THOÁT THÌ NHẤN KÝ TỰ BẤT KỲ: ")
-    if con != "y":
-        break
